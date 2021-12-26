ad923a6cb5c6ec187aa7b6d9b6c72a6d8d4af4ce
Maaz Ahmed <93466155+CodewithMaaz@users.noreply.github.com>
2021-12-26 12:54:00 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#This code has been made by code with maaz
+
+Print("Code made by code with maaz")
+
+
+# Python Program which calculate
+
+side = input(" Enter Side: ")
+side = int(side)
+
+area = side * side
+
+Text = 'Your area is %s'
+
+print(Text % area)
